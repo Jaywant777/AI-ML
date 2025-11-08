d68f66c46ee26c7b438f27fb62fe1a723fd5a936
Jaywant <121999236+Jaywant777@users.noreply.github.com>
2025-11-09 00:05:32 +0530

    Add files via upload

---
+++ Python/day11.py
@@ -0,0 +1,23 @@
+s1,s2={1,2,3,4,5},{4,5,6,7,8}
+
+# # Union
+# print(s1.union(s2))
+
+# # Intersection
+# print(s1.intersection(s2))
+
+# # Diffrence
+# print(s1.difference(s2))
+
+# # symmetric_difference
+# print(s1.symmetric_difference(s2))
+
+# intersection_update
+# s1.intersection_update(s2)
+# print(s1)
+# print(s2)
+
+# diffrence_update
+s1.difference_update(s2)
+print(s1)
+print(s2)
